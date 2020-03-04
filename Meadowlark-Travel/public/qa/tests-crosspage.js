d14e87b20da0e8fb34c5a4f8db35b02af48b8825
Tuan Duc <duclt47@gmail.com>
2020-03-04 23:55:15 +0700

    add test unit and fix test cross page

--- Meadowlark-Travel/public/qa/tests-crosspage.js
+++ Meadowlark-Travel/public/qa/tests-crosspage.js
@@ -1,43 +1,38 @@
-var Browser = require('zombie'),
-    assert = require('chai').assert;
+var Browser = require('zombie');
 
+    //Chai has some problem i use browser.assert instead of assert
+    // assert = require('chai').assert;
 var browser;
-
 suite('Cross-Page Tests', function () {
-
     setup(function () {
         browser = new Browser();
     });
-
-    test('requesting a group rate quote from the hood river tour page should ' +
-        'populate the hidden referrer field correctly', function (done) {
+    test('requesting a group rate quote from the hood river tour page' +
+        'should populate the referrer field', function (done) {
             var referrer = 'http://localhost:3000/tours/hood-river';
             browser.visit(referrer, function () {
                 browser.clickLink('.requestGroupRate', function () {
-                    console.log("aaaaaaa", browser.field('referrer').value);
-                    assert(browser.field('referrer').value === referrer);
+                    browser.assert.element('form input[name=referrer]',referrer);
                     done();
                 });
             });
         });
-
     test('requesting a group rate from the oregon coast tour page should ' +
-        'populate the hidden referrer field correctly', function (done) {
+        'populate the referrer field', function (done) {
             var referrer = 'http://localhost:3000/tours/oregon-coast';
             browser.visit(referrer, function () {
                 browser.clickLink('.requestGroupRate', function () {
-                    assert(browser.field('referrer').value === referrer);
+                    browser.assert.element('form input[name=referrer]',referrer);
                     done();
                 });
             });
         });
-
     test('visiting the "request group rate" page dirctly should result ' +
-        'in an empty value for the referrer field', function (done) {
-            browser.visit('http://localhost:3000/tours/request-group-rate', function () {
-                assert(browser.field('referrer').value === '');
-                done();
-            });
+        'in an empty referrer field', function (done) {
+            browser.visit('http://localhost:3000/tours/request-group-rate',
+                function () {
+                    browser.assert.element('form input[name=referrer]','');
+                    done();
+                });
         });
-
 });
